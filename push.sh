552d9ed32e5980cda9e7b40dc69219ea8f54a6f5
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-25 23:43:45 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Added MSTest Cases for Address Book Database"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
