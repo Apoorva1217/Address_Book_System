d943b580bb18a62de81e4111be0f9409b554841e
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-25 22:29:48 +0530

    [Apoorva] Add . Ability to update the Contact Information in the address book for a person and ensure that the Contact Information in the memory is in Sync with the DB

--- AddressBookDB/AddressBookRepo.cs
+++ AddressBookDB/AddressBookRepo.cs
@@ -71,5 +71,60 @@ namespace AddressBookDB
                 this.sqlconnection.Close();
             }
         }
+
+        /// <summary>
+        /// Ability to update the Contact Information in the address book for a person and ensure that the Contact Information in the memory is in Sync with the DB
+        /// </summary>
+        /// <param name="addressBookModel"></param>
+        public void UpdateContact(AddressBookModel addressBookModel)
+        {
+            SqlConnection sqlconnection = new SqlConnection(connectionString);
+            try
+            {
+                AddressBookModel employeeModel = new AddressBookModel();
+                using (this.sqlconnection)
+                {
+                    string query = @"UPDATE Address_Book SET Person_Address='Vashi',City='NaviMumbai',State='Maharashtra' 
+                                    WHERE First_Name='Aayush' AND Last_Name='Kadam';";
+                    SqlCommand sqlCommand = new SqlCommand(query, this.sqlconnection);
+
+                    this.sqlconnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            employeeModel.First_Name = sqlDataReader.GetString(0);
+                            employeeModel.Last_Name = sqlDataReader.GetString(1);
+                            employeeModel.Person_Address = sqlDataReader.GetString(2);
+                            employeeModel.City = sqlDataReader.GetString(3);
+                            employeeModel.State = sqlDataReader.GetString(4);
+
+
+                            Console.WriteLine("{0},{1},{2},{3},{4}",
+                                employeeModel.First_Name, employeeModel.Last_Name, employeeModel.Person_Address, employeeModel.City,
+                                employeeModel.State);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    sqlDataReader.Close();
+                    this.sqlconnection.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+            finally
+            {
+                this.sqlconnection.Close();
+            }
+        }
     }
 }
