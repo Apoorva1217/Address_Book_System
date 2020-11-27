145e0e0ff5a79b3d06fa06f990e3c2afb3235d79
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-27 20:09:17 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Add Contact to Address Book with Thread"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
