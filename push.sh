c3ce9af53824422e6c7464193f4c65f4b7934f9c
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-29 13:42:11 +0530

    [Apoorva] Add . Add Contact to Address Book with Thread with Synchronization

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ read a
 git branch $a
 git checkout $a
 git add .
-git commit -m "[Apoorva] Add . Add Contact to Address Book with Thread"
+git commit -m "[Apoorva] Add . Add Contact to Address Book with Thread with Synchronization"
 git push origin $a
 git checkout master
 git merge $a
