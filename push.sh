977bdf871a01b19a67ba3f22dfd75de4fe293b38
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-12-01 22:04:50 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Ability to Update entry in AddressBook JSON Server"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
